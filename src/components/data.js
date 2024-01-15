bbbd71c0318ead81d7712e80c7bd153656cfaf2f
imnamix <namit3040@gmail.com>
2024-01-15 22:00:35 +0530

    first committ

---
+++ src/components/data.js
@@ -0,0 +1,420 @@
+const data = [
+  {
+    id: 1,
+    question: "If cats and dogs could play video games, which game would they love the most?",
+    answers: [
+      {
+        option: "A",
+        text: "Paw-nite",
+        correct: false,
+      },
+      {
+        option: "B",
+        text: "Meow-rio Kart",
+        correct: false,
+      },
+      {
+        option: "C",
+        text: "Fetch Effect",
+        correct: true,
+      },
+      {
+        option: "D",
+        text: "Tails of Duty",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 2,
+    question: "Why did the bicycle fall over?",
+    answers: [
+      {
+        option: "A",
+        text: "It was two-tired",
+        correct: false,
+      },
+      {
+        option: "B",
+        text: "It lost its balance",
+        correct: false,
+      },
+      {
+        option: "C",
+        text: "It was too tired to stand",
+        correct: true,
+      },
+      {
+        option: "D",
+        text: "It wanted to rest-cycle",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 3,
+    question: "How do you organize a space party?",
+    answers: [
+      {
+        option: "A",
+        text: "You planet",
+        correct: false,
+      },
+      {
+        option: "B",
+        text: "Call the Milky Way",
+        correct: false,
+      },
+      {
+        option: "C",
+        text: "Throw a meteor shower",
+        correct: false,
+      },
+      {
+        option: "D",
+        text: "You asteroid friends",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 4,
+    question: "Why did the scarecrow win an award?",
+    answers: [
+      {
+        option: "A",
+        text: "It was outstanding in its field",
+        correct: true,
+      },
+      {
+        option: "B",
+        text: "It scared away crows",
+        correct: false,
+      },
+      {
+        option: "C",
+        text: "It had the best hat",
+        correct: false,
+      },
+      {
+        option: "D",
+        text: "It knew how to do the hay-cha cha",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 5,
+    question: "What do you call fake spaghetti?",
+    answers: [
+      {
+        option: "A",
+        text: "Fauxghetti",
+        correct: true,
+      },
+      {
+        option: "B",
+        text: "Impastable",
+        correct: false,
+      },
+      {
+        option: "C",
+        text: "Not-noodles",
+        correct: false,
+      },
+      {
+        option: "D",
+        text: "Spaghett-yo",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 6,
+    question: "Why did the tomato turn red?",
+    answers: [
+      {
+        option: "A",
+        text: "It saw the salad dressing",
+        correct: true,
+      },
+      {
+        option: "B",
+        text: "It was blushing",
+        correct: false,
+      },
+      {
+        option: "C",
+        text: "It was ripe",
+        correct: false,
+      },
+      {
+        option: "D",
+        text: "It was embarrassed",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 7,
+    question: "What did the janitor say when he jumped out of the closet?",
+    answers: [
+      {
+        option: "A",
+        text: "Supplies!",
+        correct: true,
+      },
+      {
+        option: "B",
+        text: "Boo!",
+        correct: false,
+      },
+      {
+        option: "C",
+        text: "Clean up on aisle 5!",
+        correct: false,
+      },
+      {
+        option: "D",
+        text: "Surprise!",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 8,
+    question: "Why did the math book look sad?",
+    answers: [
+      {
+        option: "A",
+        text: "It had too many problems",
+        correct: true,
+      },
+      {
+        option: "B",
+        text: "It failed the test",
+        correct: false,
+      },
+      {
+        option: "C",
+        text: "It was feeling divided",
+        correct: false,
+      },
+      {
+        option: "D",
+        text: "It lost its solutions",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 9,
+    question: "How do you catch a squirrel?",
+    answers: [
+      {
+        option: "A",
+        text: "Climb a tree and act like a nut",
+        correct: true,
+      },
+      {
+        option: "B",
+        text: "Set up a nut trap",
+        correct: false,
+      },
+      {
+        option: "C",
+        text: "Ask it politely",
+        correct: false,
+      },
+      {
+        option: "D",
+        text: "Use a squirrel net",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 10,
+    question: "What's orange and sounds like a parrot?",
+    answers: [
+      {
+        option: "A",
+        text: "A carrot",
+        correct: true,
+      },
+      {
+        option: "B",
+        text: "A pumpkin",
+        correct: false,
+      },
+      {
+        option: "C",
+        text: "A mango",
+        correct: false,
+      },
+      {
+        option: "D",
+        text: "A traffic cone",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 11,
+    question: "Why did the computer keep its drink on top of the monitor?",
+    answers: [
+      {
+        option: "A",
+        text: "For higher spirits",
+        correct: true,
+      },
+      {
+        option: "B",
+        text: "To stay grounded",
+        correct: false,
+      },
+      {
+        option: "C",
+        text: "To cool it down",
+        correct: false,
+      },
+      {
+        option: "D",
+        text: "To avoid spills",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 12,
+    question: "What's a vampire's favorite fruit?",
+    answers: [
+      {
+        option: "A",
+        text: "Blood orange",
+        correct: true,
+      },
+      {
+        option: "B",
+        text: "Grapes",
+        correct: false,
+      },
+      {
+        option: "C",
+        text: "Apples",
+        correct: false,
+      },
+      {
+        option: "D",
+        text: "Bananas",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 13,
+    question: "Why don't scientists trust atoms?",
+    answers: [
+      {
+        option: "A",
+        text: "Because they make up everything",
+        correct: true,
+      },
+      {
+        option: "B",
+        text: "Because they are too small",
+        correct: false,
+      },
+      {
+        option: "C",
+        text: "Because they are unstable",
+        correct: false,
+      },
+      {
+        option: "D",
+        text: "Because they are invisible",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 14,
+    question: "Why did the scarecrow become a successful motivational speaker?",
+    answers: [
+      {
+        option: "A",
+        text: "Because it was outstanding in its field",
+        correct: true,
+      },
+      {
+        option: "B",
+        text: "Because it could scare away doubts",
+        correct: false,
+      },
+      {
+        option: "C",
+        text: "Because it had a winning attitude",
+        correct: false,
+      },
+      {
+        option: "D",
+        text: "Because it knew how to stand tall",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 15,
+    question: "Why did the computer go to therapy?",
+    answers: [
+      {
+        option: "A",
+        text: "Too many bytes of emotional baggage",
+        correct: true,
+      },
+      {
+        option: "B",
+        text: "It had a hard drive of unresolved issues",
+        correct: false,
+      },
+      {
+        option: "C",
+        text: "To defragment its emotions",
+        correct: false,
+      },
+      {
+        option: "D",
+        text: "To debug its feelings",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 16,
+    question: "What's a skeleton's least favorite room in the house?",
+    answers: [
+      {
+        option: "A",
+        text: "The living room",
+        correct: true,
+      },
+      {
+        option: "B",
+        text: "The bathroom",
+        correct: false,
+      },
+      {
+        option: "C",
+        text: "The kitchen",
+        correct: false,
+      },
+      {
+        option: "D",
+        text: "The bedroom",
+        correct: false,
+      },
+    ],
+  },
+];
+
+export default data;
